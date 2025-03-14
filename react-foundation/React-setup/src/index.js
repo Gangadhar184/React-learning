52d9c6ffa0810595b6eec8a2d08f238742d93a31
Gangadhar184 <zenistuig65@gmail.com>
2025-03-14 18:21:49 +0530

    understood the jsx topic

--- react-foundation/React-setup/src/index.js
+++ react-foundation/React-setup/src/index.js
@@ -2,16 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './style.css';
 
-const heading = React.createElement("h1", {id:"title"}, "Onepiece is Greatest of all time");
+// const heading = React.createElement("h1", {id:"title"}, "Onepiece is Greatest of all time");
 
 /* 
-    
+    <div class="container">
+        <h1>onepiece<h1>
+        <p>GOAT</p>
+    </div>
 */
+const name = "Luffy"
+const isMyName = true
+const container = 
+<>
+<h1>Onepiece</h1>
+<p>{20+20}</p>
+<h2>{name}</h2>
+<h2>{isMyName ? "zoro" : "sanji"}</h2>
+</>
+
 
 const app = document.getElementById("app");
 
 const root = ReactDOM.createRoot(app);
-root.render(heading);
+root.render(container);
 
 //React + ReactDOM + ourcode => bundle.js
 //we use webpack bunlder, 
--- react-foundation/React-setup/webpack.config.js
+++ react-foundation/React-setup/webpack.config.js
@@ -21,6 +21,20 @@ module.exports = {
             {
                 test:/\.css$/,
                 use:["style-loader", "css-loader"]
+            },
+            {
+                test : /\.js|jsx|mjs|cjs$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : "babel-loader",
+                    options : {
+                        presets : 
+                            [ "@babel/preset-env",
+                                "@babel/preset-react"]
+                        
+                    }
+                }
+               
             }
         ]
     }
